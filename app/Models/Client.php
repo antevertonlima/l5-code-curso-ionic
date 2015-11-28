458db894dd3f72faf77ad8389ce996515771145d
Everton Lima <antevertonlima@gmail.com>
2015-11-28 14:12:52 -0300

    first commit

---
+++ app/Models/Client.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace CodeDelivery\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Client extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'phone',
+        'address',
+        'city',
+        'state',
+        'zipcode'
+    ];
+
+    public function user()
+    {
+        $this->hasOne(User::class);
+    }
+}
