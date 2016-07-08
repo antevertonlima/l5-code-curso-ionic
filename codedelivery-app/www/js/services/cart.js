03924f5e001c6f13d3217a0386d3c05743cf62f5
Everton Lima <antevertonlima@gmail.com>
2016-07-08 05:36:36 -0300

    Criado serviço de carrinho

--- codedelivery-app/www/js/services/cart.js
+++ codedelivery-app/www/js/services/cart.js
@@ -1,20 +1,51 @@
 angular.module('starter.services')
-.factory('$cart',['$window',
-    function($window){
-    	return {
-    		set: function(key, value){
-    			$window.localStorage[key] = value;
-    			return $window.localStorage[key];
-    		},
-    		get: function(key, defaultValue){
-    			return $window.localStorage[key] || defaultValue;
-    		},
-    		setObject: function(key, value){
-    			$window.localStorage[key] = JSON.stringify(value);
-    			return this.getObject(key);
-    		},
-    		getObject: function(key){
-    			return JSON.parse($window.localStorage[key]) || null;
-    		}
-    	}
+.service('$cart',['$localStorage',
+    function($localStorage){
+    	var key = 'cart';
+		this.clear = function () {
+			$localStorage.setObject(key,{
+				items: [],
+				total: 0
+			});
+		};
+		this.get = function () {
+			return $localStorage.getObject(key);
+		};
+		this.getItem = function (i) {
+			return this.get().items[i];
+		};
+		this.addItem = function (item) {
+			var cart = this.get(),itemAux,exists=false;
+			for (var index in cart.items){
+				itemAux = cart.items[index];
+				if(itemAux.id = item.id){
+					itemAux.qtd = item.qtd + itemAux.qtd;
+					itemAux.subTotal = calculateSubTotal(itemAux);
+					exists = true;
+					break;
+				}
+			}
+			if(!exists){
+				item.subTotal = calculateSubTotal(item);
+				cart.items.push(item);
+			}
+			cart.total = getTotal(cart.items);
+			$localStorage.getObject(key,cart);
+		};
+		this.removeItem = function (i) {
+			var cart = this.get();
+			cart.items.splice(i,1);
+			cart.total = getTotal(cart.items);
+			$localStorage.getObject(key,cart);
+		};
+		function calculateSubTotal(item){
+			return item.price * item.qtd;
+		};
+		function getTotal(items) {
+			var sum = 0;
+			angular.forEach(items, function (item) {
+				sum += item.subTotal;
+			});
+			return sum;
+		}
 }]);
